3b79b9903f28590f2bcd7a41a4cce777a2eb41f4
Puidor <c.adriano.silva@hotmail.com>
2021-11-26 16:08:14 -0300

    Atributos + Metodos

--- conta.py
+++ conta.py
@@ -1,2 +1,21 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto... {}".format(self))
+        # Define os atributos privados
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
